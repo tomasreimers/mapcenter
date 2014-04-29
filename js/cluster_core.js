d564accae8e7e7d7cf468cb5e9c7467acc5321cb
Tomas Reimers <tomasreimers@gmail.com>
2014-04-28 21:44:22 -0400

    Rewrote cluster_core

--- js/cluster_core.js
+++ js/cluster_core.js
@@ -30,7 +30,7 @@ var cluster_core = new (function () {
 			parties.push(new_party);
 		}
 		return parties;
-	}
+	};
 
 	// checks if all the parties are within a threshold
 	self.all_within_threshold = function (parties) {
@@ -46,74 +46,64 @@ var cluster_core = new (function () {
 
   	// calculates centers of k parties and returns objects with the points assigned 
   	self.cluster_with_k = function (k, points) {
-		// find create k initial parties and hold them in an 'parties' array
-		parties = self.pick_starting_centers (k, points);
+		
+		// get starting centers
+		var parties = self.pick_starting_centers(k, points);
 
-		// 'i' now references the index of the unclustered points.
-		// walk through the remains of the points array and cluster them
-
-		for (var i = 0; i < points.length; i++) {
-			// check to see which cluster is closest to the current point
-			var closest_party = parties[0];
-			var closest_dist = self.distance_between(closest_party,points[i]);
-			for (var p = 1; p < parties.length; p++) {
-				dist = self.distance_between(parties[p], points[i]);
-				if (dist < closest_dist) {
-					closest_dist = dist;
-					closest_party = parties[p];
-				}
-			}
-			// add point to the closest party and adjust the mean
-			closest_party.points.push(points[i]);
-			self.adjust_center(closest_party);
+		// reassign iteratively
+		for (var i = 0; i < self.ITERATIONS; i++) {
+			var new_centers = self.clean_centers(parties);
+			parties = self.assign_to_parties(points, new_centers);
 		}
+
 		// Now reapportion the points to the parties based on new means
-		self.reapportion(parties, self.ITERATIONS);
-		// Return final list of parties
+		return parties;
+		
+ 	};
+
+ 	self.assign_to_parties = function (points, parties) {
+ 		// assign everything a party to start
+		_.each(points, function (point) {
+			var closest_party = undefined;
+			var closest_dist = 0;
+			_.each(parties, function (party) {
+				var dist = self.distance_between(party, point);
+				if (dist < closest_dist || typeof(closest_party) == "undefined") {
+					closest_dist = dist;
+					closest_party = party;
+				}
+			});
+			// add point to the closest party and adjust the mean
+			closest_party.points.push(point);
+			self.adjust_center(closest_party);
+		});
 		return parties;
  	};
 
-  // recursively reassign points to new parties
-  self.reapportion = function (parties, limit) {
-		// Walk through each point in each party and double check its position
-		for (var p = 0; p < parties.length; p++) {
-			var party = parties[p];
-			for (var i = 0; i < party.points.length; i++) {
-				var point = party.points[i];
-				// Check the distance of this point to each center
-				var cur_dist = self.distance_between(party, point);
-				// Compare with each other party
-				for (var p2 = 0; p2 < parties.length; p2++) {
-					if (p2 != p) { // Dont check against same party
-						var other_dist = self.distance_between(parties[p2], point);
-						if (other_dist < cur_dist) {
-							// The point needs to change parties
-							point.dist = other_dist;
-							party.points.splice(i,1); // removes from party
-							parties[p2].points.push(point); // add to closer party
-							self.adjust_center(party); // recalcuate mean
-							self.adjust_center(parties[p2]); // and again
-							if (limit > 0)
-								self.reapportion(parties, limit-1) // recurse
-						}
-					}
-				}
-			}
-		}
-	}
+ 	self.clean_centers = function (parties) {
+ 		var new_parties = [];
+ 		_.each(parties, function (party) {
+ 			new_parties.push({
+		    	lat: party.lat,
+		    	lng: party.lng,
+		    	points: []
+			});
+ 		});
+ 		return new_parties;
+ 	};
 
 	self.distance_between = function (p1, p2) {
 		var width = p1.lng - p2.lng;
 		var height = p1.lat - p2.lat;
 		var dist = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
 		return dist;
-	}
+	};
 
 	self.adjust_center = function (party) {
 		var latSum = _.reduceRight(party.points, function(acc, p) {return acc + p.lat}, 0);
 		var lngSum = _.reduceRight(party.points, function(acc, p) {return acc + p.lng}, 0);
 		party.lat = latSum / party.points.length;
 		party.lng = lngSum / party.points.length;
-	}
+	};
 
 });
